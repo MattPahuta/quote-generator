6b46b3a4898a505973b33d8b5cefca939ac26221
Matt Haines <mhpahuta@gmail.com>
2022-02-23 15:12:43 -0800

    Add readme, minor code refactor

---
+++ README.md
@@ -0,0 +1,21 @@
+# Random Quote Generator - Zero To Mastery JS Project
+
+This is a solution to a Zero To Mastery JavaScript Web Project.
+
+## Overview
+
+Users should be able to:
+
+- View a random quote upon page load
+- Get a new quote by selecting the 'New Quote' button
+- Generate a tweet with the current quote and author when selecting the Twitter button
+
+### Screenshot
+
+![Random Quote](./quote-gen-ss.png)
+
+### Built with 
+
+- Vanilla JavaScript
+- Flexbox
+- [Inspirational Quote API](https://quotes-react.netlify.app/)
--- script.js
+++ script.js
@@ -8,21 +8,21 @@ const loader = document.getElementById('loader');
 // get quotes from API
 let apiQuotes = [];
 
-// show loading
-function loading() {
+function showLoadingSpinner() {
   loader.hidden = false;
   quoteContainer.hidden = true;
 }
 
-// hide loading
-function complete() {
-  quoteContainer.hidden = false;
-  loader.hidden = true;
+function removeLoadingSpinner() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
 }
 
 // show new quote
 function newQuote() {
-  loading();
+  showLoadingSpinner();
   // pick random quote
   const randomIndex = Math.floor(Math.random() * apiQuotes.length);
   const quote = apiQuotes[randomIndex];
@@ -30,6 +30,8 @@ function newQuote() {
   // check if author field is blank
   if (!quote.author) {
     authorText.textContent = 'Unknown';
+  } else if (quote.author === 'Donald Trump') {
+    authorText.textContent = 'Cheeto Criminal D-Bag';
   } else {
     authorText.textContent = quote.author;
   }
@@ -40,15 +42,13 @@ function newQuote() {
     quoteText.classList.remove('long-quote');
   }
 
-  // handle DJT quote(s)
-
   // set quote and hide loader
   quoteText.textContent = quote.text;
-  complete();
+  removeLoadingSpinner();
 }
 
 async function getQuotes() {
-  loading();
+  showLoadingSpinner();
   const apiUrl = 'https://type.fit/api/quotes';
   try {
     const response = await fetch(apiUrl);
@@ -56,7 +56,7 @@ async function getQuotes() {
     // console.log(apiQuotes[12]);
     newQuote();
   } catch (error) {
-    // catch error 
+    getQuotes();
   }
 }
 
