c98c30103267fc2371925f8a192e850b5ded639a
Matt Haines <mhpahuta@gmail.com>
2022-02-23 13:55:19 -0800

    Add loading animation and markup

--- script.js
+++ script.js
@@ -3,12 +3,26 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 // get quotes from API
 let apiQuotes = [];
 
+// show loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// hide loading
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 // show new quote
 function newQuote() {
+  loading();
   // pick random quote
   const randomIndex = Math.floor(Math.random() * apiQuotes.length);
   const quote = apiQuotes[randomIndex];
@@ -26,10 +40,15 @@ function newQuote() {
     quoteText.classList.remove('long-quote');
   }
 
+  // handle DJT quote(s)
+
+  // set quote and hide loader
   quoteText.textContent = quote.text;
+  complete();
 }
 
 async function getQuotes() {
+  loading();
   const apiUrl = 'https://type.fit/api/quotes';
   try {
     const response = await fetch(apiUrl);
